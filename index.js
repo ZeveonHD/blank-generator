5448d7c8bffb561e91aec17618e72e015c67de94
Ryan Overdeer <roverdeer@outlook.com>
2024-08-26 19:33:54 -0700

    Setup
    - Imported path
    - Imported fs
    - Imported express
    - Imported body parser
    
    - Created app.listen on specified PORT variable

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const fs = require('fs')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const PORT = 3000
+
+app.listen(PORT, (err) => {
+    if (err){
+        throw err
+    }
+
+    console.log(`Server is running on port ${PORT}`)
+})

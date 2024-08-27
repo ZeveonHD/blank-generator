7f0d249b65abfbff7e742408dbefb7a735f3b167
Ryan Overdeer <roverdeer@outlook.com>
2024-08-26 20:45:51 -0700

    Width
    Width calculations

--- generator.js
+++ generator.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const fs = require('path')
 const jimp = require('jimp')
-const fraction = require('fraction').FractionStream
+const { Fraction } = require('fractional')
+const fraction = require('fractional').Fraction
+
 
 const defaultWidth = 300
 const defaultHeight = 300
@@ -10,15 +12,6 @@ const defaultBackground = "#000000"
 const defaultColor = "#FFFFFF"
 const defaultStroke = "#0C0C0C"
 const defaultText = "300x300"
-
-function getAspect(options = {
-    width: null,
-    height: null
-}){
-    if (width === null || height === null){
-        return 'N/A'
-    }
-}
 
 function calculateAspect(options ={
     width: null,
@@ -29,7 +22,10 @@ function calculateAspect(options ={
     }
 
     var decimal = width/height
-    var fraction = new Frac
+    var fraction = new Fraction(decimal)
+    var string = fraction.numerator + ':' + fraction.denominator
+
+    return string
 }
 
 const generateBlankImage = (options = {
@@ -41,17 +37,30 @@ const generateBlankImage = (options = {
     stroke: null,
     text: null
 }) => {
+    // no width given
     if (options.width === null){
+        // no width or height given
         if (options.height === null){
             options.width=300
             options.height=300
             options.aspect='1:1'
         }
+        // no width but height given
         else if (options.height !== null){
+            // no aspect given
             if (options.aspect === null){
                 options.width=defaultWidth
-                options.aspect
+                options.aspect=calculateAspect({width: options.width, height: options.height})
+            }
+            // aspect given 
+            else {
+                var words=options.aspect.split(':')
+                var num=words[0] // width aspect
+                options.width=options.height*num
             }
         }
+    }
+    else if (options.height === null){
+        
     }
 }
